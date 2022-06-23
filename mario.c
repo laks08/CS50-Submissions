ef5cb1d5c17ede842394af57295187ca7b826b53
laks08 <laks08@users.noreply.github.com>
2022-06-23 11:41:33 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -4,23 +4,23 @@
 int main(void)
 {
     //variable and input >0
-    int a=0;
+    int a = 0;
     do
     {
         a = get_int("Enter number of rows:");
     }
-    while((a<1) || (a>8));
+    while ((a < 1) || (a > 8));
     //for loop i for rows, i=0 to avoid start printing equal to 'a' spaces.
-    for(int i = 0; i < a ; i++)
+    for (int i = 0; i < a ; i++)
     {
         //for loop j for rows, a-i for reducing the no. of spaces each cycle
-        for(int j = 1; j < a-i; j++)
+        for (int j = 1; j < a - i; j++)
         {
             printf(" ");
         }
         //while for increasing # each row wrt to i, i+1 for cycle 0
         int count = 0;
-        while(count < (i+1))
+        while (count < (i + 1))
         {
             printf("#");
             count++;
