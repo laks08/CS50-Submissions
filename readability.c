74a3906fd41e343f009bf32247ee2d0a6f11154e
laks08 <laks08@users.noreply.github.com>
2022-06-27 12:52:05 +0000

    automated commit by submit50

--- readability.c
+++ readability.c
@@ -13,8 +13,8 @@ int main(void)
     int x = count_letters(text);
     float w = count_words(text);
     int y = count_sentences(text);
-    float l = (x/w)*100;
-    float s = (y/w)*100;
+    float l = (x / w) * 100;
+    float s = (y / w) * 100;
 
     // printf("%i\n",x);
     // printf("%f\n",w);
@@ -27,15 +27,15 @@ int main(void)
     //printf("Index:%i\n",index);
     if (index > 1 && index < 16)
     {
-      printf("Grade %i\n", index);
+        printf("Grade %i\n", index);
     }
     else if (index >= 16)
     {
-      printf("Grade 16+\n");
+        printf("Grade 16+\n");
     }
     else
     {
-      printf("Before Grade 1\n");
+        printf("Before Grade 1\n");
     }
 
 }
@@ -46,40 +46,40 @@ int count_letters(string s)
     int sum = 0;
     for (int i = 0; i < n; i++)
     {
-      int a = (int)s[i];
-      if ((a > 64 && a < 91)||(a > 96 && a < 123))
-      {
-        sum++;
-      }
+        int a = (int)s[i];
+        if ((a > 64 && a < 91) || (a > 96 && a < 123))
+        {
+            sum++;
+        }
     }
     return sum;
 }
 
 float count_words(string s)
 {
-  int n = strlen(s);
-  int sum = 0;
-  for (int i =0;i<n;i++)
-  {
-    if (s[i]==' ')
+    int n = strlen(s);
+    int sum = 0;
+    for (int i = 0 ; i < n; i++)
     {
-      sum++;
+        if (s[i] == ' ')
+        {
+            sum++;
+        }
     }
-  }
-  sum = sum + 1;
-  return sum;
+    sum = sum + 1;
+    return sum;
 }
 
 int count_sentences(string s)
 {
-  int n = strlen(s);
-  int sum = 0;
-  for (int i =0;i<n;i++)
-  {
-    if((s[i]=='.')||(s[i]=='!')||(s[i]=='?'))
+    int n = strlen(s);
+    int sum = 0;
+    for (int i = 0; i < n ; i++)
     {
-      sum++;
+        if ((s[i] == '.') || (s[i] == '!') || (s[i] == '?'))
+        {
+            sum++;
+        }
     }
-  }
-  return sum;
+    return sum;
 }
