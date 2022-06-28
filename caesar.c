6e9ef5e841bc19af460fc44b0263a64a71600516
laks08 <laks08@users.noreply.github.com>
2022-06-28 13:05:00 +0000

    automated commit by check50 [check50=True]

---
+++ caesar.c
@@ -0,0 +1,70 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+bool only_digits(string s);
+char rotate(char c, int n);
+
+int main(int argc, string argv[])
+{
+    string l  = argv[1];
+    bool x = only_digits(l);
+    return x;
+    int a = atoi(l);
+    string s = get_string("plaintext:");
+    int sl = strlen(s);
+    char ciphertext[sl];
+    for(int i = 0; i<sl; i++)
+    {
+       ciphertext[i] = rotate(s[i],a);
+    }
+     printf("Cipher:%s\n",ciphertext);
+}
+
+bool only_digits(string s)
+{
+
+    if(isdigit(s[0]))
+    {
+        return 0;
+    }
+    else if(isalpha(s[0])||isalnum(s[0])||isspace(s[0]))
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+    else
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+}
+
+char rotate(char c, int n)
+{
+    if (isupper(c))
+    {
+        int p = (int)c;
+        int p1 = p - 65;
+        int p2 = (p1+n)%26;
+        int p3 = p2 + 65;
+        char ci = (char)p3;
+        return ci;
+    }
+    else if (islower(c))
+    {
+        int p = (int)c;
+        int p1 = p - 97;
+        int p2 = (p1+n)%26;
+        int p3 = p2 + 97;
+        char ci = (char)p3;
+        return ci;
+    }
+    else
+    {
+        return c;
+    }
+}
+
