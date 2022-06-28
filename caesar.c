b6f04a74d0299f7e9d574c377198224d133a2ff6
laks08 <laks08@users.noreply.github.com>
2022-06-28 15:44:48 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -9,31 +9,55 @@ char rotate(char c, int n);
 
 int main(int argc, string argv[])
 {
-    string l  = argv[1];
-    bool x = only_digits(l);
-    return x;
-    int a = atoi(l);
-    string s = get_string("plaintext:");
-    int sl = strlen(s);
-    char ciphertext[sl];
-    for(int i = 0; i<sl; i++)
+    //printf("argc val:%i\n",argc);
+    if(argc < 2 || argc > 2)
     {
-       ciphertext[i] = rotate(s[i],a);
+        printf("Usage: ./caesar key\n");
+        return 1;
     }
-     printf("Cipher:%s\n",ciphertext);
-}
-
-bool only_digits(string s)
-{
-
-    if(isdigit(s[0]))
+    string l  = argv[1];
+    //printf("l value:%c\n",l[0]);
+    bool x =  only_digits(l);
+    if(x==0)
     {
+        int a = atoi(l);
+        string s = get_string("plaintext:  ");
+        int sl = strlen(s);
+        //printf("sl:%i",sl);
+        char ciphertext[sl];
+        for(int i = 0; i < sl; i++)
+        {
+            ciphertext[i] = rotate(s[i],a);
+        }
+        printf("ciphertext: %s\n",ciphertext);
         return 0;
     }
     else
     {
         printf("Usage: ./caesar key\n");
         return 1;
+    }
+
+}
+bool only_digits(string z)
+{
+    int n = strlen(z);
+    int a=0;
+    for(int i=0;i<n;i++)
+    {
+        if (isalpha(z[i]))
+        {
+            a++;
+        }
+    }
+
+    if(a>0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
     }
 }
 
@@ -50,12 +74,12 @@ char rotate(char c, int n)
     }
     else if (islower(c))
     {
-        int p = (int)c;
-        int p1 = p - 97;
-        int p2 = (p1+n)%26;
-        int p3 = p2 + 97;
-        char ci = (char)p3;
-        return ci;
+        int q = (int)c;
+        int q1 = q - 97;
+        int q2 = (q1+n)%26;
+        int q3 = q2 + 97;
+        char ci1 = (char)q3;
+        return ci1;
     }
     else
     {
