c006829bb5824b743f2dfcb0f006de361e7b52cd
laks08 <laks08@users.noreply.github.com>
2022-07-01 13:55:15 +0000

    automated commit by submit50

--- substitution.c
+++ substitution.c
@@ -12,49 +12,49 @@ int main(int argc, string argv[])
         printf("Error! Enter 1 key!\n");
         return 1;
     }
-        string s = argv[1];
-        int n = strlen(s);// string key check
-        bool x = arg_check(s);
-        //printf("bool value:%d",x);
-        if (x==1)
+    string s = argv[1];
+    int n = strlen(s);
+    bool x = arg_check(s);// string key check
+    //printf("bool value:%d",x);
+    if (x == 1)
+    {
+        //printf("Error! Enter correct key!\n");
+        return x; // return 1 if conditions not met
+    }
+    else
+    {
+        for (int i = 0; i < n ; i++)
         {
-            //printf("Error! Enter correct key!\n");
-            return x;
+            s[i] = toupper(s[i]);// change string to upper case irresespective of the key case
         }
-        else
+        string plaintext = get_string("plaintext:");
+        int n1 = strlen(plaintext);
+        char c[n1];
+        for (int i = 0; i < n1; i++)
         {
-            for(int i=0; i<n ; i++)
+            int a = (int)plaintext[i];
+            if ((a > 64) && (a < 91)) // sub for upper case text
             {
-                s[i] = toupper(s[i]);
+                int b = a - 65;
+                c[i] = s[b];
             }
-            string plaintext = get_string("plaintext:");
-            int n1 = strlen(plaintext);
-            char c[n1];
-            for (int i =0; i < n1; i++)
+            else if ((a > 96) && (a < 123)) //sub for lower case text
             {
-                int a = (int)plaintext[i];
-                if ((a > 64) && (a < 91))
-                {
-                    int b = a - 65;
-                    c[i] = s[b];
-                }
-                else if ((a > 96) && (a < 123))
-                {
-                    int f = a - 97;
-                    c[i] = tolower(s[f]);
-                }
-                else
-                {
-                    c[i] = plaintext[i];
-                }
+                int f = a - 97;
+                c[i] = tolower(s[f]);
             }
-            printf("ciphertext:");
-            for (int j = 0; j < n1; j++)
+            else
             {
-                printf("%c",c[j]);
+                c[i] = plaintext[i]; // else condition for other chars like space and periods, ! etc.
             }
-            printf("\n");
         }
+        printf("ciphertext:");
+        for (int j = 0; j < n1; j++)
+        {
+            printf("%c", c[j]); // print cipher text
+        }
+        printf("\n");
+    }
 }
 
 bool arg_check(string str)
@@ -63,20 +63,20 @@ bool arg_check(string str)
     int check = 0;
     int check1 = 0;
 
-    for (int i = 0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = i+1; j < n;j++)
+        for (int j = i + 1; j < n; j++)
         {
-            if(str[j] == str[i])
+            if (str[j] == str[i]) //check for duplicates
             {
                 check++;
             }
         }
     }
 
-    for (int k = 0; k<n ; k++)
+    for (int k = 0; k < n ; k++)
     {
-        if(!isalpha(str[k]))
+        if (!isalpha(str[k])) //check if all is alpha
         {
             //printf("STEP:%i\n",k);
             //printf("str:%c\n",str[k]);
@@ -84,7 +84,7 @@ bool arg_check(string str)
         }
     }
 
-    if((n==26) && (check == 0) && (check1 == 0))
+    if ((n == 26) && (check == 0) && (check1 == 0))
     {
         return 0;
     }
