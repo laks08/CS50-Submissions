393bff63cb9b049d54115587a90369b459da2234
laks08 <laks08@users.noreply.github.com>
2022-08-27 07:04:29 +0000

    automated commit by check50 [check50=True]

--- mario.py
+++ mario.py
@@ -2,7 +2,7 @@
 
 while True:
     height = int(input("Height: "))  # input height for user
-    if (height < 0) and (height > 9):
+    if (height > 0) and (height < 9):
         break
 
 for i in range(height):  # loop for height times
