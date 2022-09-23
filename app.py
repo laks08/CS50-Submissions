dc02a2921c1ae7cedf3009c812b899756c0b237d
laks08 <laks08@users.noreply.github.com>
2022-09-23 22:36:34 +0000

    automated commit by check50 [check50=True]

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def buy():
 
     else:
         symbol = request.form.get("symbol")
-        shares = float(request.form.get("shares"))
+        shares = int(request.form.get("shares"))
 
         if not symbol:
             return apology("Symbol Needed")
@@ -72,7 +72,7 @@ def buy():
         if stock == None:
             return apology("Symbol does not exist")
 
-        if shares < 0 or not shares.is_integer():
+        if shares < 0:
             return apology("Shares not allowed")
 
         transaction_value = shares * stock["price"]
@@ -262,7 +262,7 @@ def sell():
 
     else:
         symbol = request.form.get("symbol")
-        shares = request.form.get("shares")
+        shares = int(request.form.get("shares"))
 
         if not symbol:
             return apology("Symbol Needed")
